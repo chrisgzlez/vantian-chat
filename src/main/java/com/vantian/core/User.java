ac55346153b895eda38d974c9f8222d9df1022ed
Christian Gonzalez <christiangv68@gmail.com>
2023-12-06 00:12:12 +0100

    feat: prop user implementation

--- src/main/java/com/vantian/core/User.java
+++ src/main/java/com/vantian/core/User.java
@@ -1,9 +1,21 @@
 package com.vantian.core;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  * User
  */
-public class User {
+public class User extends UnicastRemoteObject implements IUser {
+    private String userName;
+    public User(String name) throws RemoteException {
+        super();
 
-    
+        this.userName = name;
+        return;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    } 
 }
