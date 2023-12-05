a486f4c9d64030b321c20ee20c03ef3ce9b6366f
Christian Gonzalez <christiangv68@gmail.com>
2023-12-05 23:47:39 +0100

    chore: created GUI

---
+++ src/main/java/com/vantian/gui/GUI.java
@@ -0,0 +1,17 @@
+package com.vantian.gui;
+
+import com.vantian.core.IUserManager;
+
+/**
+ * GUI
+ * Se encarga de inicializar los componentes de la GUI todos,
+ * Comunicar con los servicios del backend
+ * ...
+ */
+public class GUI {
+
+    public GUI(IUserManager userManager) {
+        return;
+    }
+    
+}
