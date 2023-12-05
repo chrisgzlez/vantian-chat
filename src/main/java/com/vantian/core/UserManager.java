36f242df1883ec14d5973803b29841ef6975d6e2
Christian Gonzalez <christiangv68@gmail.com>
2023-12-05 20:00:40 +0100

    chore: created user manager implementation

---
+++ src/main/java/com/vantian/core/UserManager.java
@@ -0,0 +1,28 @@
+package com.vantian.core;
+import java.rmi.*;
+import java.rmi.server.*;
+
+/**
+ * UserManager
+ */
+public class UserManager extends UnicastRemoteObject implements IUserManager {
+    UserManager() throws RemoteException {
+        return;
+    }
+
+    public boolean signIn(IUser user) throws RemoteException{
+        return true;
+    }
+
+    public boolean logIn(IUser user) throws RemoteException{
+        return true;
+    }
+
+    public boolean isRegistered(IUser user) throws RemoteException{
+        return true;
+    }
+
+}
+
+
+
