2ef422db85faef08d4717aa46f262e2433e3197a
Christian Gonzalez <christiangv68@gmail.com>
2023-12-06 00:11:32 +0100

    feat: prop user sign in

--- src/main/java/com/vantian/core/UserManager.java
+++ src/main/java/com/vantian/core/UserManager.java
@@ -7,18 +7,28 @@ import java.rmi.server.*;
  */
 public class UserManager extends UnicastRemoteObject implements IUserManager {
     public UserManager() throws RemoteException {
+        super();
         return;
     }
 
-    public boolean signIn(IUser user) throws RemoteException{
+    public boolean signIn(IUser user) {
+        try {
+            //TODO: Implemente
+            System.out.println("Sign in called by: " + user.getUserName());
+            System.out.println("Reference: " + user.toString());
+            return true;
+            
+        } catch (Exception e) {
+            System.out.println(" [x] Could not reach user...");
+            return false;
+        }
+    }
+
+    public boolean logIn(IUser user) {
         return true;
     }
 
-    public boolean logIn(IUser user) throws RemoteException{
-        return true;
-    }
-
-    public boolean isRegistered(IUser user) throws RemoteException{
+    public boolean isRegistered(IUser user) {
         return true;
     }
 
