f0640ca384165d6a1aeee5e5744985eb76b91c59
Christian Gonzalez <christiangv68@gmail.com>
2023-12-06 22:55:38 +0100

    chore: created statements in interface

--- src/main/java/com/vantian/bd/IDBManager.java
+++ src/main/java/com/vantian/bd/IDBManager.java
@@ -1,10 +1,16 @@
 package com.vantian.bd;
 
+import java.sql.PreparedStatement;
+
 /**
  * IDBManager
  */
 public interface IDBManager {
 
     public IDataBaseConnection getConnection();
+
+    public PreparedStatement isRegisteredStmt();
+
+    public PreparedStatement createUserStmt();
     
 }
