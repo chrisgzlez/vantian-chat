7b87007a323c9705e489dd6d58b4f344fc9fc65f
Christian Gonzalez <christiangv68@gmail.com>
2023-12-06 03:24:45 +0100

    feat: implemented constructor and has been initialized

--- src/main/java/com/vantian/bd/PostgresInitializer.java
+++ src/main/java/com/vantian/bd/PostgresInitializer.java
@@ -4,9 +4,21 @@ package com.vantian.bd;
  */
 public class PostgresInitializer {
     private String database;
-    private boolean intialized;
+    private boolean intialized = false;
 
     public PostgresInitializer(String database) {
         this.database = database;
+        this.init();
+    }
+
+    public boolean hasBeenInitialized() {
+        return this.intialized;
+    }
+
+    //TODO: Implement Initialization
+    // Create Database if not exists
+    // Create Tables if not exists
+    public void init() {
+        return;
     }
 }
