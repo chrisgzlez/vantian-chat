d591213c61c0bb3d9e63648e82549ffb25540bdd
Christian Gonzalez <christiangv68@gmail.com>
2023-12-06 22:59:19 +0100

    feat: implemented sign in and log in

--- src/main/java/com/vantian/core/UserManager.java
+++ src/main/java/com/vantian/core/UserManager.java
@@ -1,21 +1,51 @@
 package com.vantian.core;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import com.vantian.bd.IDBManager;
+import com.vantian.bd.IDataBaseConnection;
 
 /**
  * UserManager
  */
 public class UserManager extends UnicastRemoteObject implements IUserManager {
-    public UserManager() throws RemoteException {
+    private IDBManager dbManager;
+    private PreparedStatement isRegisteredStmnt, createUserStmnt;
+    public UserManager(IDBManager dbManager) throws RemoteException {
         super();
+        this.dbManager = dbManager;
+
+        IDataBaseConnection conn = this.dbManager.getConnection();
+
+
+        if (this.createUserStmnt == null || this.isRegisteredStmnt == null) {
+            System.err.println(" [x] Error Connecting to Database in UserManager.");
+            System.exit(1);
+        }
         return;
     }
 
-    public boolean signIn(IUser user) {
+    public boolean signIn(IUser user, IPassword passwd) {
         try {
-            //TODO: Implemente
-            System.out.println("Sign in called by: " + user.getUserName());
+            System.out.println("Sign in called by: " + user.getUserName() + " password: " + passwd.get());
             System.out.println("Reference: " + user.toString());
+            if(this.isRegistered(user)) {
+                return true;
+            }
+
+            PreparedStatement statement = this.dbManager.createUserStmt();
+            statement.setString(1, user.getUserName());
+            statement.setString(2, passwd.get());
+            statement.setTimestamp(3, Timestamp.from(Instant.now()));
+            statement.setTimestamp(4, Timestamp.from(Instant.now()));
+            ResultSet rs = statement.executeQuery();
+            return rs.next();
             return true;
             
         } catch (Exception e) {
@@ -24,13 +54,24 @@ public class UserManager extends UnicastRemoteObject implements IUserManager {
         }
     }
 
-    public boolean logIn(IUser user) {
+    public boolean logIn(IUser user, IPassword passwd) {
         return true;
     }
 
     public boolean isRegistered(IUser user) {
-        return true;
+        PreparedStatement statement = this.dbManager.isRegisteredStmt();
+        try {
+            statement.setString(1, user.getUserName());
+            ResultSet rs = statement.executeQuery();
+
+            // If registered, returns true else false
+            return rs.next();
+        } catch (Exception e) {
+            System.out.println(" [x] Could not reach user...");
+            return false;
+        }
     }
+
 
 }
 
