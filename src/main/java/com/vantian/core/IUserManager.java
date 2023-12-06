73fd295de10d5bdbc5ff9c77b7468d6427e1bd13
Christian Gonzalez <christiangv68@gmail.com>
2023-12-06 22:57:26 +0100

    fix: add password to sign in and log in

--- src/main/java/com/vantian/core/IUserManager.java
+++ src/main/java/com/vantian/core/IUserManager.java
@@ -6,8 +6,8 @@ import java.rmi.*;
  * Manejo de Usuarios
  */
 public interface IUserManager extends Remote {
-    public boolean signIn(IUser user) throws RemoteException;
-    public boolean logIn(IUser user) throws RemoteException;
+    public boolean signIn(IUser user, IPassword passwd) throws RemoteException;
+    public boolean logIn(IUser user, IPassword passwd) throws RemoteException;
     public boolean isRegistered(IUser user) throws RemoteException;
 }
 
