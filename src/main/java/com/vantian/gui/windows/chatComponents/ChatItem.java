17e0c884db80c0256e4d0f715ca6fb53eccdc53f
ivanquintans <ivanqg21@gmail.com>
2023-12-06 12:04:45 +0100

    chore: ventana basica creada, falta poder escribir y esas cosas

---
+++ src/main/java/com/vantian/gui/windows/chatComponents/ChatItem.java
@@ -0,0 +1,65 @@
+package com.vantian.gui.windows.chatComponents;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.border.EmptyBorder;
+
+public class ChatItem extends javax.swing.JLayeredPane {
+
+    private JLabel label;
+
+    public ChatItem() {
+        initComponents();
+        txt.setEditable(false);
+        txt.setBackground(new Color(0, 0, 0, 0));
+        txt.setOpaque(false);
+    }
+
+    public void setText(String text) {
+        txt.setText(text);
+        txt.setForeground(new Color(15,15, 28, 255));
+    }
+
+    public void setTime(String time) {
+        JLayeredPane layer = new JLayeredPane();
+        layer.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        layer.setBorder(new EmptyBorder(0, 5, 10, 5));
+        label = new JLabel(time);
+        label.setForeground(new Color(15, 15, 28, 158));
+        label.setHorizontalTextPosition(JLabel.LEFT);
+        layer.add(label);
+        add(layer);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txt = new com.vantian.gui.windows.swingitems.JIMSendTextPane();
+
+        setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.PAGE_AXIS));
+
+        txt.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 5, 10));
+        txt.setSelectionColor(new java.awt.Color(7, 2, 2));
+        add(txt);
+    }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        super.paintComponent(grphcs);
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.vantian.gui.windows.swingitems.JIMSendTextPane txt;
+    // End of variables declaration//GEN-END:variables
+}
