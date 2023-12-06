057e51f61382229a2bb686af0776d31eefa4225e
Christian Gonzalez <christiangv68@gmail.com>
2023-12-06 23:35:10 +0100

    feat: implemented update of friends hashmap

--- src/main/java/com/vantian/core/User.java
+++ src/main/java/com/vantian/core/User.java
@@ -2,20 +2,31 @@ package com.vantian.core;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 /**
  * User
  */
 public class User extends UnicastRemoteObject implements IUser {
     private String userName;
+    private HashMap<String, IUser> loggedFriends;
     public User(String name) throws RemoteException {
         super();
 
         this.userName = name;
+        this.loggedFriends = new HashMap<>();
         return;
     }
 
     public String getUserName() {
         return this.userName;
     } 
+
+    public void notifyLogin(IUser user) throws RemoteException {
+        this.loggedFriends.put(user.getUserName(), user);
+    }
+
+    public void updateFriends(HashMap<String, IUser> friends) throws RemoteException {
+        this.loggedFriends = friends;
+    }
 }
