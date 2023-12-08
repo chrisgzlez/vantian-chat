eaa302ce62db0eefd790daabcc9d409af42a0c87
Christian Gonzalez <christiangv68@gmail.com>
2023-12-08 18:09:56 +0100

    fix: rename to run server and delete clean run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function run() {
+function run_server() {
     Class=$1
     port=5601
     if [[ -n ${2} ]]; then
@@ -19,11 +19,6 @@ function run() {
         -Dsun.stderr.encoding=UTF-8 \
         -classpath /home/chris/usc/3GrEI/codis/vantian-chat/target/classes:/home/chris/.m2/repository/org/postgresql/postgresql/42.7.0/postgresql-42.7.0.jar:/home/chris/.m2/repository/org/checkerframework/checker-qual/3.31.0/checker-qual-3.31.0.jar:/home/chris/.m2/repository/com/formdev/flatlaf/3.2.5/flatlaf-3.2.5.jar:/home/chris/.m2/repository/com/formdev/flatlaf-extras/3.2.5/flatlaf-extras-3.2.5.jar:/home/chris/.m2/repository/com/github/weisj/jsvg/1.2.0/jsvg-1.2.0.jar:/home/chris/.m2/repository/com/formdev/flatlaf-fonts-roboto/2.137/flatlaf-fonts-roboto-2.137.jar:/home/chris/.m2/repository/com/miglayout/miglayout-swing/11.2/miglayout-swing-11.2.jar:/home/chris/.m2/repository/com/miglayout/miglayout-core/11.2/miglayout-core-11.2.jar:/home/chris/usc/3GrEI/codis/vantian-chat/src/main/resources/libraries/swing-glasspane-popup-1.2.0.jar:/home/chris/usc/3GrEI/codis/vantian-chat/src/main/resources/libraries/swing-toast-notifications-1.0.1.jar \
         com.vantian.${Class} ${port}
-}
-
-function clean_run() {
-    clean || true
-    run $1 $2
 }
 
 function build() {
