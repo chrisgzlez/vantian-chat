b89f5331529c8f3bbb5b9b238dc3ca579900e90f
Christian Gonzalez <christiangv68@gmail.com>
2023-12-09 14:19:48 +0100

    fix: iuser hierarchy
    IUser extends ICommunicate and ICommunicate extends Remote

--- src/main/java/com/vantian/core/communication/ICommunicate.java
+++ src/main/java/com/vantian/core/communication/ICommunicate.java
@@ -1,14 +1,17 @@
 package com.vantian.core.communication;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * ICommunicate
  */
-public interface ICommunicate {
+public interface ICommunicate extends Remote {
     
-    public void send(ICommunicate sender, IMessage mssg);
+    public void send(ICommunicate sender, IMessage mssg) throws RemoteException;
 
-    public IMessage receive(ICommunicate sender);
+    public IMessage receive(ICommunicate sender) throws RemoteException;
 
-    public String getId();
+    public String getId() throws RemoteException;
     
 }
