2b17d876d3ddf2b159a56d2f5ba5e80a9cae0ab8
Christian Gonzalez <christiangv68@gmail.com>
2023-12-09 14:46:02 +0100

    test: extend client main test

--- src/main/java/com/vantian/Client.java
+++ src/main/java/com/vantian/Client.java
@@ -7,6 +7,8 @@ import com.vantian.core.IUserManager;
 import com.vantian.core.Password;
 import com.vantian.core.IUser;
 import com.vantian.core.User;
+import com.vantian.core.communication.IMessage;
+import com.vantian.core.communication.TextMessage;
 import com.vantian.gui.GUI;
 
 import java.io.*;
@@ -34,6 +36,7 @@ public class Client {
             System.out.println(" [*] Initializing GUI...");
             GUI gui = new GUI(userManager);
 
+            System.out.println(" [*] SignIn & Login of Christian");
             IUser user = new User("christian");
             boolean check = userManager.signIn(user, new Password("test"));
             if(check) {
@@ -48,21 +51,36 @@ public class Client {
             System.out.println(" [*] Attempting login with wrong password");
             userManager.logIn(user, new Password("fail"));
 
+            System.out.println(" [*] SignIn & Login of Ivan");
             IUser user1 = new User("ivan");
             userManager.signIn(user1, new Password("test"));
             userManager.logIn(user1, new Password("test"));
 
+            
+            System.out.println(" [*] SignIn & Login of Adrian");
             IUser user2 = new User("adrian");
             userManager.signIn(user2, new Password("test"));
             userManager.logIn(user2, new Password("test"));
 
+            System.out.println(" [*] SignIn & Login of Loly");
             IUser user3 = new User("loly");
             userManager.signIn(user3, new Password("test"));
             userManager.logIn(user3, new Password("test"));
 
+            System.out.println(" [*] SignIn & Login of Pepe");
             IUser user4 = new User("pepe");
             userManager.signIn(user4, new Password("test"));
             userManager.logIn(user4, new Password("test"));
+
+            System.out.println("Sending Messages");
+
+            user1.send(user2, new TextMessage("Hola como estas?"));
+            user1.send(user2, new TextMessage("QUe tal el viaje?"));
+            user1.send(user2, new TextMessage("Me trajiste algun recuerdo?"));
+            for(IMessage mssg = user1.receive(user2); mssg != null; mssg = user1.receive(user2))  {
+                System.out.println("  - " + user1.getId() + " > " + user2.getId() + ": " + mssg.get());
+            }
+
         } catch (Exception e) {
             System.out.println(" [x] Exception in Client. " + e);
             e.printStackTrace(System.err);
